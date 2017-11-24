6a9ab2dfc0a473144dec3ab2a2466eaf7b4b8507
Evan Reeves <evanreeves22@gmail.com>
2017-11-24 20:32:36 +1100

    extensions

--- DataCompress.cs
+++ DataCompress.cs
@@ -9,7 +9,7 @@ namespace Game.Shared.Utility
     //Used for compressing data to send over the network.
     public class DataCompress
     {
-
+        //Not really compression, but it can be used to store more values if you limit the byte inputs. i.e. if you never input 0, only 1+, you can still use the short as a byte.
         public static ushort TwoBytesToUShort(byte a, byte b)
         {
             var s = (ushort)((a << 8) | b); //AAAAAAAABBBBBBBB
---
+++ GenericExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Shared.Utility
+{
+    public static class GenericExtensions
+    {
+        /// <summary>
+        /// Quick way to try get a value from a dictionary.
+        /// </summary>
+        /// <typeparam name="TK"></typeparam>
+        /// <typeparam name="TV"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static TV GetValue<TK, TV>(this IDictionary<TK, TV> dict, TK key, TV defaultValue = default(TV))
+        {
+            TV value;
+            return dict.TryGetValue(key, out value) ? value : defaultValue;
+        }
+    }
+}
