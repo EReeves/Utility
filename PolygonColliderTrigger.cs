3527a986f0db8c2fd9a9cf1ba3cdbad613c6b88e
ereeves <evanreeves22@gmail.com>
2017-12-10 20:11:17 +1100

    debug rendering stuff

---
+++ PolygonColliderTrigger.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using Nez.PhysicsShapes;
+
+namespace Game.Shared.Utility
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Just so that it looks different in debug render.
+    /// </summary>
+    public class PolygonColliderTrigger : PolygonCollider
+    {
+        public PolygonColliderTrigger(Vector2[] points) : base(points)
+        {
+            isTrigger = true;
+        }
+
+        public PolygonColliderTrigger(int vertCount, float radius) : base(vertCount, radius)
+        {
+            isTrigger = true;
+        }
+
+        public override void debugRender(Graphics graphics)
+        {
+            var poly = shape as Polygon;
+            // ReSharper disable once PossibleNullReferenceException
+            graphics.batcher.drawPolygon( shape.position, poly.points, Color.AliceBlue, true, Debug.Size.lineSizeMultiplier );
+            
+        }
+    }
+}
